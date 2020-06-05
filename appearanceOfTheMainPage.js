ae3ed9cd922bff24f8e42bb67ce43b1701d06c0c
Kurantsov <g.e.kurantsov@gmail.com>
2020-06-05 19:45:24 +0300

    feat: add templates to track initial information

---
+++ appearanceOfTheMainPage.js
@@ -0,0 +1,33 @@
+function languagePage() {
+  const language = document.querySelector('.language');
+  const ruLang = language.querySelector('#RU');
+  const enLang = language.querySelector('#EN');
+  ruLang.onclick = () => {
+    ruLang.classList.add('active');
+    enLang.classList.remove('active');
+  };
+
+  enLang.onclick = () => {
+    enLang.classList.add('active');
+    ruLang.classList.remove('active');
+  };
+}
+
+languagePage();
+
+function degreesPage() {
+  const degrees = document.querySelector('.degrees');
+  const fDegrees = degrees.querySelector('#F');
+  const cDegrees = degrees.querySelector('#C');
+  fDegrees.onclick = () => {
+    fDegrees.classList.add('active');
+    cDegrees.classList.remove('active');
+  };
+
+  cDegrees.onclick = () => {
+    cDegrees.classList.add('active');
+    fDegrees.classList.remove('active');
+  };
+}
+
+degreesPage();
--- getInformationToWeather.js
+++ getInformationToWeather.js
@@ -28,5 +28,3 @@ function startSearch() {
 }
 
 startSearch();
-
-export default addWeatherInformation();
